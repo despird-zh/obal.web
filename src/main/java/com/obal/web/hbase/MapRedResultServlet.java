23c99e189ed39375b3c8f17c9823193e6f2a83d2
despird-zh <despird.zhang@yahoo.com>
2014-12-13 00:06:44 +0800

    up

--- src/main/java/com/obal/web/hbase/MapRedResultServlet.java
+++ src/main/java/com/obal/web/hbase/MapRedResultServlet.java
@@ -55,9 +55,10 @@ public class MapRedResultServlet extends HttpServlet{
 		Deserializer<Result> deserializer = rstSerialUtil.getDeserializer(Result.class);
 		String uri = request.getRequestURI();
 		uri = uri.substring(request.getContextPath().length());
-		int i = count++;
-		System.out.println("start receive "+i+" th result.");
+		
 		InputStream ins = request.getInputStream();
+		String index = request.getHeader("dataindex");
+		System.out.println("start receive "+index+" th result.");
 		 // Transfer bytes from in to out
 //	    byte[] buf = new byte[1024];
 //	    int len;
@@ -69,19 +70,20 @@ public class MapRedResultServlet extends HttpServlet{
 //	    System.out.println();
 		DataOutputBuffer doutbuf = new DataOutputBuffer();
 		IOUtils.copy(ins, doutbuf);
+		System.out.println("length of data "+index+" :"+doutbuf.getLength());
 		DataInputBuffer dinbuf = new DataInputBuffer();
 		dinbuf.reset(doutbuf.getData(),doutbuf.getLength());
 		deserializer.open(dinbuf);
 		Result result = deserializer.deserialize(new Result());
 		for (KeyValue keyValue : result.raw()) {  
-            System.out.println("Column:" + new String(keyValue.getFamily())
+            System.out.println("--=["+index+"] Column:" + new String(keyValue.getFamily())
             		+ "/Qualifier:" + new String(keyValue.getQualifier())       
             		+ "/Val:" + new String(keyValue.getValue()));
         }  
 		PrintWriter w = response.getWriter();
 		w.write("__DONE__");
 		response.flushBuffer();
-		System.out.println("end receive "+i+"th result");
+		System.out.println("end receive "+index+" th result");
 		LOGGER.debug(uri.toString());
 	}
 }
